c4ff988123118f567eeb6145b5a2406b3298065e
832960 <55299454+832960@users.noreply.github.com>
2020-10-13 15:55:20 -0700

    It works

--- Maps/src/Alphabet.java
+++ Maps/src/Alphabet.java
@@ -1,39 +1,8 @@
-import java.util.ArrayList;
-
 public class Alphabet {
 
-    private final ArrayList<Integer> code = new ArrayList<>();
+    Alphabet() {
 
-    Alphabet(String text) {
 
-        for (int i = 0; i < text.length(); i++) {
-
-            code.add(getValue(text.toLowerCase().charAt(i)));
-
-        }
-
-    }
-
-    public int getValue(char ch) {
-
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        for (int i = 0; i < alphabet.length(); i++) {
-
-            if (ch == alphabet.charAt(i)) {
-
-                return i;
-
-            }
-
-        }
-
-        return -1;
-
-    }
-
-    public ArrayList<Integer> getCode() {
-
-        return code;
 
     }
 
--- Maps/src/Main.java
+++ Maps/src/Main.java
@@ -2,9 +2,59 @@ import java.util.HashMap;
 
 public class Main {
 
+    public static String cipher(HashMap<Letter, Letter> caesarCipher, String text) {
+
+        StringBuilder cipherText = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+
+            if (text.charAt(i) == ' ') {
+
+                cipherText.append(' ');
+
+            }
+
+            else if (Character.toUpperCase(text.charAt(i)) == text.charAt(i)) {
+
+                cipherText.append(Character.toUpperCase(caesarCipher.get(
+                        new Letter(Character.toLowerCase(text.charAt(i)))).letter));
+
+            }
+
+            else {
+
+                cipherText.append(caesarCipher.get(new Letter(Character.toLowerCase(text.charAt(i)))));
+
+            }
+
+        }
+
+        return cipherText.toString();
+
+    }
+
     public static void main(String[] args) {
 
-        HashMap<Alphabet, Cipher> caesarMap = new HashMap<>();
+        String text = "Hello World";
+        int shift = -1;
+
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        HashMap<Letter, Letter> caesarCipher = new HashMap<>();
+        for (int i = 0; i < alphabet.length(); i++) {
+
+            while (shift < 0) {
+
+                shift += 26;
+
+            }
+
+            caesarCipher.put(new Letter(alphabet.charAt(i)), new Letter(alphabet.charAt((i + shift) % 26)));
+
+        }
+
+        String cipherText = cipher(caesarCipher, text);
+        System.out.println(text);
+        System.out.println(cipherText);
 
     }
 
